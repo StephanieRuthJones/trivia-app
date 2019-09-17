1822e57202b8af560267cb648c6dcdc6ccc3b634
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-09-17 10:51:19 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //fetch url: http://http://jservice.io/api/clues
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Trivia</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+
+          </ul>
+        </div>
+      </nav> */}
+
+      {/* Card:  */}
+      {/* <div className="card">
+        <img src="https://momentohospitality.com.au/hillside-hotel/wp-content/uploads/sites/8/2019/05/Trivia_Tuesdays_Fbnews-1.jpg" className="card-img-top" />
+        <div className="card-body">
+          <h5 className="card-title">Question #: </h5>
+          <p className="card-text">QUESTION TEXT GOES HERE</p>
+          <p>Answer: </p>
+        </div>
+      </div> */}
+
     </div>
   );
 }
