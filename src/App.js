6d1a5dc51f740c22bec721283f1530b6bf874c8f
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-09-17 14:09:36 -0600

    Completed code w event listener on answer button

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar'
+import CardContainer from './components/CardContainer'
 function App() {
   //fetch url: http://http://jservice.io/api/clues
 
   return (
     <div className="App">
-      {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Trivia</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
+      <Navbar />
+      <CardContainer />
 
-          </ul>
-        </div>
-      </nav> */}
-
-      {/* Card:  */}
-      {/* <div className="card">
-        <img src="https://momentohospitality.com.au/hillside-hotel/wp-content/uploads/sites/8/2019/05/Trivia_Tuesdays_Fbnews-1.jpg" className="card-img-top" />
-        <div className="card-body">
-          <h5 className="card-title">Question #: </h5>
-          <p className="card-text">QUESTION TEXT GOES HERE</p>
-          <p>Answer: </p>
-        </div>
-      </div> */}
+     
 
     </div>
   );
